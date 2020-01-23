a412fb39944ec4fe99b16b346eb67301c3cc092b
Davi Viana <davipviana@gmail.com>
2020-01-23 06:53:19 -0300

    Initial project

---
+++ src/Azure.SasTokenGenerator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Azure.SasTokenGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
